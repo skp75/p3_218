3e6ed7975f1d48f94edb26eb09ab01fad0c363ab
skp75 <skp75@njit.edu>
2022-04-29 21:40:29 -0400

    csv form

--- app/auth/forms/__init__.py
+++ app/auth/forms/__init__.py
@@ -3,6 +3,12 @@ from wtforms import validators
 from wtforms.fields import *
 
 
+class csv_form(FlaskForm):
+    file = FileField('Upload your CSV file', [
+        validators.DataRequired(),
+    ], description='Submit your CSV file!')
+    submit = SubmitField()
+
 class login_form(FlaskForm):
     email = EmailField('Email Address', [
         validators.DataRequired(),
